266b47f8faa207d0a369d7e98031d91f2db97e9d
Robert Hawk <robert.hawk@gmail.com>
2025-07-06 07:19:56 +0000

    feat: Initial frontend functionality

---
+++ web/components/ActivityList.vue
@@ -0,0 +1,30 @@
+<template>
+<DataTable :value="activities"> <!--table-style="min-width: 50rem">-->
+  <Column field="unit" header="Unit"/>
+  <Column field="name" header="Activity Name"/>
+  <Column field="type" header="Activity Type"/>
+  <Column field="start" header="Start Date"/>
+  <Column field="end" header="End Date"/>
+  <Column header="">
+    <template #body="slotProps"><NuxtLink :to="`/activity/${slotProps.data.id}`">View</NuxtLink></template>
+  </Column>
+</DataTable>
+</template>
+
+<script setup lang="ts">
+const activities = [
+  {
+    id: "dhxxii",
+    unit: "RMR-UT-001",
+    name: "Desert Hawk XXII",
+    type: "Encampment",
+    start: "25 June 2025",
+    end: "3 July 2025",
+  }
+]
+
+</script>
+
+<style scoped>
+
+</style>
--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,8 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const a5Preset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{blue.50}',
+      100: '{blue.100}',
+      200: '{blue.200}',
+      300: '{blue.300}',
+      400: '{blue.400}',
+      500: '{blue.500}',
+      600: '{blue.600}',
+      700: '{blue.700}',
+      800: '{blue.800}',
+      900: '{blue.900}',
+      950: '{blue.950}',
+    }
+  }
+});
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
@@ -10,6 +29,18 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+    '@primevue/nuxt-module',
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: a5Preset,
+        options: {
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    },
+  },
+//  css: ['~/assets/css/main.css'],
 })
